8f0920720387bfdd1ed704c8391314c55b8b5708
GerarditoGonzalito <gerardossgonzalez72@gmail.com>
2025-03-13 12:28:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,17 @@
-// service-worker.js
+const CACHE_NAME = "images-cache-v1";
 
-// Nombre del caché
-const CACHE_NAME = 'images-cache-v1';
-
-// Instalar el Service Worker
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado");
 });
 
-// Activar el Service Worker
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activado');
-  // Limpiar cachés viejos si es necesario
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Borrando caché obsoleto:', cacheName);
+            console.log("Borrando caché obsoleto:", cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -26,24 +20,21 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Manejar las solicitudes de red (fetch) para imágenes
-self.addEventListener('fetch', (event) => {
-  // Solo manejar imágenes
-  if (event.request.url.includes('.jpg') || event.request.url.includes('.png') || event.request.url.includes('.gif')) {
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes(".jpg") || event.request.url.includes(".png") || event.request.url.includes(".gif")) {
     event.respondWith(
-      caches.match(event.request) // Primero, busca en el caché
+      caches.match(event.request)
         .then((cachedResponse) => {
           if (cachedResponse) {
-            return cachedResponse; // Si la imagen está en caché, la sirve
+            return cachedResponse;
           }
           
-          // Si no está en caché, la descargamos de la red y la almacenamos en caché
           return fetch(event.request)
             .then((response) => {
               if (response.ok) {
                 caches.open(CACHE_NAME)
                   .then((cache) => {
-                    cache.put(event.request, response.clone()); // Guardar la imagen en caché
+                    cache.put(event.request, response.clone());
                   });
                 return response;
               }
